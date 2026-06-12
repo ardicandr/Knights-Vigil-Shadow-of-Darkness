003d91fc2e374fd88b74682d179e2d4f9c148b80
ardicandr <153357487+ardicandr@users.noreply.github.com>
2026-06-12 20:10:17 +0700

    Penambahan tombol dan panel di scene main menu

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -4,9 +4,17 @@ using System.Collections;
 
 public class MainMenuController : MonoBehaviour
 {
+    [Header("Level Settings")]
     [SerializeField] public string firstLevelName = "Shadowshire Village";
+    
+    [Header("Transition Settings")]
     public CanvasGroup fadeGroup;
     public float fadeDuration = 1f;
+
+    [Header("UI Panels")]
+    public GameObject settingsPanel;
+    public GameObject creditsPanel;
+    public GameObject quitConfirmPanel;
 
     void Start()
     {
@@ -15,6 +23,11 @@ public class MainMenuController : MonoBehaviour
         
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+
+        // Tutup semua panel saat awal
+        if (settingsPanel != null) settingsPanel.SetActive(false);
+        if (creditsPanel != null) creditsPanel.SetActive(false);
+        if (quitConfirmPanel != null) quitConfirmPanel.SetActive(false);
 
         if (fadeGroup != null) StartCoroutine(Fade(0));
     }
@@ -28,13 +41,33 @@ public class MainMenuController : MonoBehaviour
     {
         if (fadeGroup != null) yield return StartCoroutine(Fade(1));
         LevelData.StoredHealth = -1f;
-
         LevelData.NextLevelName = firstLevelName;
         SceneManager.LoadScene("LoadingScene");
     }
 
-    public void QuitGame()
-    {
+    public void OpenSettings() {
+        settingsPanel.SetActive(true);
+    }
+    public void CloseSettings() {
+        settingsPanel.SetActive(false);
+    }
+
+    public void OpenCredits() {
+        creditsPanel.SetActive(true);
+    }
+    public void CloseCredits() {
+        creditsPanel.SetActive(false);
+    }
+
+    public void ShowQuitConfirmation() {
+        quitConfirmPanel.SetActive(true);
+    }
+
+    public void HideQuitConfirmation() {
+        quitConfirmPanel.SetActive(false);
+    }
+
+    public void FinalQuit() {
         Debug.Log("Game ditutup.");
         Application.Quit();
     }
